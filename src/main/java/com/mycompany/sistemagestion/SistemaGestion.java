7b9499708f9aa35b2313277da01da342cd1abaf5
lnxxxxxxxx <leandro.luna25@hotmail.com>
2023-01-26 10:32:50 -0300

    first commit

---
+++ src/main/java/com/mycompany/sistemagestion/SistemaGestion.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestion;
+
+/**
+ *
+ * @author pachi
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
